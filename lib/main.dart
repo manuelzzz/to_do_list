7ea91f10ae7f08fc3bb7c0a6396faf4fa94a1c6f
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-17 11:29:06 -0300

    adding the firebase_core and firebase_auth dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/app/app_module.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const AppModule());
 }
