6d703a00ffbbc88e2194bbdf626be5c761e04960
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-17 13:01:48 -0300

    Adding password and confirm password validator

---
+++ lib/app/core/validators/validators.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Validators {
+  Validators._();
+
+  static FormFieldValidator compare(
+      TextEditingController? valueEC, String message) {
+    return (value) {
+      final valueCompare = valueEC?.text ?? '';
+      if (value == null || (value != null && value != valueCompare)) {
+        return message;
+      } else {}
+    };
+  }
+}
