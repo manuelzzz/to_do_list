d9c7ffc6aa7ad054ed3bdd79c2d5364a7fe619aa
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-19 17:31:25 -0300

    Login Controller update to recieve the user service

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,4 +1,3 @@
-import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/modules/todo_list_module.dart';
 import 'package:todo_list/app/modules/auth/login/login_controller.dart';
@@ -11,7 +10,9 @@ class AuthModule extends TodoListModule {
       : super(
           bindings: [
             ChangeNotifierProvider(
-              create: (_) => LoginController(),
+              create: (context) => LoginController(
+                userService: context.read(),
+              ),
             ),
             ChangeNotifierProvider(
               create: (context) => RegisterController(
