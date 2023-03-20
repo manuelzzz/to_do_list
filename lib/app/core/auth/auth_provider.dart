a98753a9fb4ecba2192c9c594de10ed085d57e78
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-20 17:05:59 -0300

    Authprovider implementation

---
+++ lib/app/core/auth/auth_provider.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/navigator/todo_list_navigator.dart';
+import 'package:todo_list/app/services/user/user_service.dart';
+
+class AuthProvider extends ChangeNotifier {
+  final FirebaseAuth _firebaseAuth;
+  final UserService _userService;
+
+  AuthProvider({
+    required FirebaseAuth firebaseAuth,
+    required UserService userService,
+  })  : _firebaseAuth = firebaseAuth,
+        _userService = userService;
+
+  Future<void> logout() => _userService.logout();
+
+  User? get user => _firebaseAuth.currentUser;
+
+  void loadListener() {
+    _firebaseAuth.userChanges().listen((_) => notifyListeners());
+    _firebaseAuth.idTokenChanges().listen((user) {
+      if (user != null) {
+        TodoListNavigator.to.pushNamedAndRemoveUntil('/home', (route) => false);
+      } else {
+        TodoListNavigator.to
+            .pushNamedAndRemoveUntil('/login', (route) => false);
+      }
+    });
+  }
+}
