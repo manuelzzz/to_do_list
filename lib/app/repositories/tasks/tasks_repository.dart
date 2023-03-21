85e96a78a2099d957780a6dad96ceb45d8edc00d
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-21 16:21:43 -0300

    The tasks repositories part to save and search for filter on home page

--- lib/app/repositories/tasks/tasks_repository.dart
+++ lib/app/repositories/tasks/tasks_repository.dart
@@ -1,3 +1,6 @@
+import 'package:todo_list/app/models/task_model.dart';
+
 abstract class TasksRepository {
-  Future<void> save (DateTime date, String description);
+  Future<void> save(DateTime date, String description);
+  Future<List<TaskModel>> findByPeriod(DateTime start, DateTime end);
 }
--- lib/app/repositories/tasks/tasks_repository_impl.dart
+++ lib/app/repositories/tasks/tasks_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:todo_list/app/core/database/sqlite_connection_factory.dart';
+import 'package:todo_list/app/models/task_model.dart';
 import 'package:todo_list/app/repositories/tasks/tasks_repository.dart';
 
 class TasksRepositoryImpl implements TasksRepository {
@@ -20,5 +21,26 @@ class TasksRepositoryImpl implements TasksRepository {
         'finalizado': 0
       },
     );
+  }
+
+  @override
+  Future<List<TaskModel>> findByPeriod(DateTime start, DateTime end) async {
+    final startFilter = DateTime(start.year, start.month, start.day, 0, 0, 0);
+    final endFilter = DateTime(end.year, end.month, end.day, 23, 59, 59);
+
+    final conn = await _sqliteConnectionFactory.openConnection();
+    final result = await conn.rawQuery(
+      '''
+        select *
+        from todo
+        where data_hora between ? and ?
+        oder by data_hora
+        ''',
+      [
+        startFilter.toIso8601String(),
+        endFilter.toIso8601String(),
+      ],
+    );
+    return result.map((e) => TaskModel.loadFromDB(e)).toList();
   }
 }
