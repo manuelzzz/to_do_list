efa9b38d1c62dea799bdceb86b2e3a87b6edbce3
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-21 16:22:23 -0300

    The tasks service implementation to behave with repository the save and search on filters

--- lib/app/services/tasks/tasks_service.dart
+++ lib/app/services/tasks/tasks_service.dart
@@ -1,3 +1,9 @@
+import 'package:todo_list/app/models/task_model.dart';
+import 'package:todo_list/app/models/week_task_model.dart';
+
 abstract class TasksService {
-  Future<void> save (DateTime date, String description);
+  Future<void> save(DateTime date, String description);
+  Future<List<TaskModel>> getToday();
+  Future<List<TaskModel>> getTomorrow();
+  Future<WeekTaskModel> getWeek();
 }
--- lib/app/services/tasks/tasks_service_impl.dart
+++ lib/app/services/tasks/tasks_service_impl.dart
@@ -1,3 +1,5 @@
+import 'package:todo_list/app/models/task_model.dart';
+import 'package:todo_list/app/models/week_task_model.dart';
 import 'package:todo_list/app/repositories/tasks/tasks_repository.dart';
 import 'package:todo_list/app/services/tasks/tasks_service.dart';
 
@@ -10,4 +12,35 @@ class TasksServiceImpl implements TasksService {
   @override
   Future<void> save(DateTime date, String description) =>
       _tasksRepository.save(date, description);
+
+  @override
+  Future<List<TaskModel>> getToday() {
+    return _tasksRepository.findByPeriod(DateTime.now(), DateTime.now());
+  }
+
+  @override
+  Future<List<TaskModel>> getTomorrow() {
+    var tomorrowDate = DateTime.now().add(const Duration(days: 1));
+    return _tasksRepository.findByPeriod(tomorrowDate, tomorrowDate);
+  }
+
+  @override
+  Future<WeekTaskModel> getWeek() async {
+    final today = DateTime.now();
+    var startFilter = DateTime(today.year, today.month, today.day, 0, 0, 0);
+    DateTime endFilter;
+
+    if (startFilter.weekday != DateTime.monday) {
+      startFilter.subtract(Duration(days: (startFilter.weekday - 1)));
+    }
+    endFilter = startFilter.add(const Duration(days: 7));
+
+    final tasks = await _tasksRepository.findByPeriod(startFilter, endFilter);
+
+    return WeekTaskModel(
+      startDate: startFilter,
+      endDate: endFilter,
+      tasks: tasks,
+    );
+  }
 }
