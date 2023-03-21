4d50d4e366871a108f61b0bacd8cdac64ac18868
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-21 12:46:06 -0300

    LocalizationDelegates and routes update on appWidget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo_list/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list/app/core/navigator/todo_list_navigator.dart';
 import 'package:todo_list/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list/app/modules/auth/auth_module.dart';
 import 'package:todo_list/app/modules/home/home_module.dart';
 import 'package:todo_list/app/modules/splash/splash_page.dart';
+import 'package:todo_list/app/modules/tasks/task_module.dart';
 
 class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -37,8 +39,16 @@ class _AppWidgetState extends State<AppWidget> {
       routes: {
         ...AuthModule().routers,
         ...HomeModule().routers,
+        ...TaskModule().routers
       },
       navigatorKey: TodoListNavigator.navigatorKey,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt', 'BR'),
+      ],
       home: const SplashPage(),
     );
   }
