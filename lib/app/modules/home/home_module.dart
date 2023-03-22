3903f214adae792b41a98c41018bac0f5ff66532
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-22 18:53:59 -0300

    Home page att, with module update, init state config, bugs fix and visual changes

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -2,17 +2,35 @@ import 'package:provider/provider.dart';
 import 'package:todo_list/app/core/modules/todo_list_module.dart';
 import 'package:todo_list/app/modules/home/home_controller.dart';
 import 'package:todo_list/app/modules/home/home_page.dart';
+import 'package:todo_list/app/repositories/tasks/tasks_repository.dart';
+import 'package:todo_list/app/repositories/tasks/tasks_repository_impl.dart';
+import 'package:todo_list/app/services/tasks/tasks_service.dart';
+import 'package:todo_list/app/services/tasks/tasks_service_impl.dart';
 
 class HomeModule extends TodoListModule {
   HomeModule()
       : super(
           bindings: [
+            Provider<TasksRepository>(
+              create: (context) => TasksRepositoryImpl(
+                sqliteConnectionFactory: context.read(),
+              ),
+            ),
+            Provider<TasksService>(
+              create: (context) => TasksServiceImpl(
+                tasksRepository: context.read(),
+              ),
+            ),
             ChangeNotifierProvider(
-              create: (context) => HomeController(),
+              create: (context) => HomeController(
+                tasksService: context.read(),
+              ),
             ),
           ],
           router: {
-            '/home': (context) => const HomePage(),
+            '/home': (context) => HomePage(
+                  homeController: context.read(),
+                ),
           },
         );
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/app/core/notifier/todo_list_listener_notifier.dart';
+
 import 'package:todo_list/app/core/ui/theme_extensions.dart';
 import 'package:todo_list/app/core/ui/todo_list_icons.dart';
+import 'package:todo_list/app/models/task_filter_enum.dart';
+import 'package:todo_list/app/modules/home/home_controller.dart';
 import 'package:todo_list/app/modules/home/widgets/home_drawer.dart';
 import 'package:todo_list/app/modules/home/widgets/home_filters.dart';
 import 'package:todo_list/app/modules/home/widgets/home_header.dart';
@@ -8,12 +12,43 @@ import 'package:todo_list/app/modules/home/widgets/home_tasks.dart';
 import 'package:todo_list/app/modules/home/widgets/home_week_filter.dart';
 import 'package:todo_list/app/modules/tasks/task_module.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  final HomeController _homeController;
 
-  void _goToCreateTask(BuildContext context) {
+  const HomePage({
+    Key? key,
+    required HomeController homeController,
+  })  : _homeController = homeController,
+        super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    TodoListListenerNotifier(changeNotifier: widget._homeController).listener(
+      context: context,
+      successCallback: (notifier, listenerInsatance) {
+        listenerInsatance.dispose();
+      },
+    );
+
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        widget._homeController.loadTotalTasks();
+        widget._homeController.findTasks(
+          filter: TaskFilterEnum.today,
+        );
+      },
+    );
+  }
+
+  Future<void> _goToCreateTask(BuildContext context) async {
     // Navigator.of(context).pushNamed('/task/create');
-    Navigator.of(context).push(
+    await Navigator.of(context).push(
       PageRouteBuilder(
         transitionDuration: const Duration(milliseconds: 350),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
@@ -33,6 +68,7 @@ class HomePage extends StatelessWidget {
         },
       ),
     );
+    widget._homeController.refreshPage();
   }
 
   @override
@@ -46,10 +82,16 @@ class HomePage extends StatelessWidget {
         elevation: 0,
         actions: [
           PopupMenuButton(
+            onSelected: (value) {
+              widget._homeController.showOrHideFinishingTasks();
+            },
             icon: const Icon(TodoListIcons.filter, size: 20),
             itemBuilder: (_) => [
-              const PopupMenuItem<bool>(
-                child: Text('Mostrar tarefas concluidas'),
+              PopupMenuItem<bool>(
+                value: true,
+                child: Text(
+                  '${widget._homeController.showFinishingTasks ? "Ocultar" : "Mostrar"} tarefas concluidas',
+                ),
               ),
             ],
           ),
