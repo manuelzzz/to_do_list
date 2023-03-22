6e084a2999a50f32f9dd52e1b1bef71a0da65910
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-22 18:47:23 -0300

    Adding the TaskFilterDescription extension on TaskFilterEnum to return today, tomorrow or this week

--- lib/app/models/task_filter_enum.dart
+++ lib/app/models/task_filter_enum.dart
@@ -3,3 +3,16 @@ enum TaskFilterEnum {
   tomorrow,
   week,
 }
+
+extension TaskFilterDescriptionExtension on TaskFilterEnum {
+  String get description {
+    switch (this) {
+      case TaskFilterEnum.today:
+        return 'DE HOJE';
+      case TaskFilterEnum.tomorrow:
+        return 'DE AMANHÃ';
+      case TaskFilterEnum.week:
+        return 'DA SEMANA';
+    }
+  }
+}
