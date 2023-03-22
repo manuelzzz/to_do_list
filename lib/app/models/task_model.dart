ed67ce8227a321e46ed40d200993bbdd34b60d3f
Manuel Santos Souza <manuelss.23.bil@gmail.com>
2023-03-22 18:48:09 -0300

    Task model class copy with add

--- lib/app/models/task_model.dart
+++ lib/app/models/task_model.dart
@@ -19,4 +19,18 @@ class TaskModel {
       finished: task['finalizado'] == 1,
     );
   }
+
+  TaskModel copyWith({
+    int? id,
+    String? description,
+    DateTime? dateTime,
+    bool? finished,
+  }) {
+    return TaskModel(
+      id: id ?? this.id,
+      description: description ?? this.description,
+      dateTime: dateTime ?? this.dateTime,
+      finished: finished ?? this.finished,
+    );
+  }
 }
